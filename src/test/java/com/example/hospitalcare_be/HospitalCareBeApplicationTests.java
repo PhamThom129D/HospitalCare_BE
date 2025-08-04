87634cda542fb9d1498305f7eb190cbade1eccd3
NgocThom <thomd1209@gmail.com>
2025-08-04 10:16:26 +0700

    first commit

---
+++ src/test/java/com/example/hospitalcare_be/HospitalCareBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospitalcare_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalCareBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
