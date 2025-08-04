d54708955765e9bfd6cfd30fba3e42a1534fc100
NgocThom <thomd1209@gmail.com>
2025-08-04 21:24:55 +0700

    config lib

---
+++ src/main/java/com/example/hospitalcare_be/sevice/OtpService.java
@@ -0,0 +1,43 @@
+package com.example.hospitalcare_be.service;
+
+import com.example.hospitalcare_be.dto.OtpVerification;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+
+@Service
+public class OtpService {
+    private final HttpSession session;
+    private final SecureRandom random = new SecureRandom();
+
+    public OtpService(HttpSession session) {
+        this.session = session;
+    }
+
+    public OtpVerification generateOtp(Long accountId) {
+        String otpCode = String.format("%06d", random.nextInt(1_000_000));
+        LocalDateTime expiresAt = LocalDateTime.now().plusMinutes(3);
+
+        OtpVerification otp = new OtpVerification(accountId, otpCode,LocalDateTime.now(), expiresAt,true);
+        otp.setCreatedAt(LocalDateTime.now());
+
+        session.setAttribute("OTP_" + accountId, otp);
+        return otp;
+    }
+
+    public boolean verifyOtp(Long accountId, String code) {
+        OtpVerification otp = (OtpVerification) session.getAttribute("OTP_" + accountId);
+        if (otp == null) return false;
+
+        boolean notExpired = LocalDateTime.now().isBefore(otp.getExpiresAt());
+        boolean match = otp.getOtpCode().equals(code) && notExpired;
+
+        if (match) {
+            session.removeAttribute("OTP_" + accountId);
+        }
+
+        return match;
+    }
+}
